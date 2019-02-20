d681d69834506a6cb3d89cac630926b2c706f795
FrostyTheKillMan <44824554+FrostyTheKillMan@users.noreply.github.com>
2019-02-20 08:04:55 -0800

    Update settings.lua

--- scripts/vscripts/settings.lua
+++ scripts/vscripts/settings.lua
@@ -268,13 +268,11 @@ function GameMode:OnHeroInGame(hero)
 	hero.playerName = PlayerResource:GetPlayerName(hero:GetPlayerID())
 	-- Store this hero handle in this table.
 	table.insert(self.vPlayers, hero)
-
-	-- This line for example will set the starting gold of every hero to 500 unreliable gold
-	hero:SetGold(500, false)
-
+	
+	hero:SetHPGain( 8 )
+	hero:SetArmorGain( 0.15000 )
+	
 	-- These lines will create an item and add it to the player, effectively ensuring they start with the item
-	local item = CreateItem("item_example_item", hero, hero)
-	hero:AddItem(item)
 end
 
 --[[
